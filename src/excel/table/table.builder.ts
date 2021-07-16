60e324c3715eb2d1d82441e254fdee3dc77e179c
Vinh Van <cvin59@gmail.com>
2021-07-16 12:29:35 +0700

    refactor table.builder.ts

--- src/excel/table/table.builder.ts
+++ src/excel/table/table.builder.ts
@@ -1,45 +1,44 @@
 import { Table, TableColumnProperties, TableStyleProperties } from 'exceljs'
 
-
 export default class TableBuilder {
     private readonly table: Table
 
-    name(name: string): TableBuilder {
+    name(name: string): this {
         this.table.name = name
         return this
     }
 
-    displayName(displayName: string): TableBuilder {
+    displayName(displayName: string): this {
         this.table.displayName = displayName
         return this
     }
 
-    ref(ref: string): TableBuilder {
+    ref(ref: string): this {
         this.table.ref = ref
         return this
     }
 
-    headerRow(headerRow: boolean): TableBuilder {
+    headerRow(headerRow: boolean): this {
         this.table.headerRow = headerRow
         return this
     }
 
-    totalsRow(totalsRow: boolean): TableBuilder {
+    totalsRow(totalsRow: boolean): this {
         this.table.totalsRow = totalsRow
         return this
     }
 
-    style(style: TableStyleProperties): TableBuilder {
+    style(style: TableStyleProperties): this {
         this.table.style = style
         return this
     }
 
-    columns(columns: TableColumnProperties[]): TableBuilder {
+    columns(columns: TableColumnProperties[]): this {
         this.table.columns = columns
         return this
     }
     
-    rows(rows: any[][]): TableBuilder {
+    rows(rows: any[][]): this {
         this.table.rows = rows
         return this
     }
